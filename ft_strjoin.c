4b4266a17bcb41d3f03bbfb4f172b4d1fa332c4e
Adrien LECLERCQ <adlecler@e1r3p16.42.fr>
2021-12-03 15:36:07 +0100

    new

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,18 @@
 /*   By: adlecler <adlecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 18:56:44 by adlecler          #+#    #+#             */
-/*   Updated: 2021/12/02 18:58:16 by adlecler         ###   ########.fr       */
+/*   Updated: 2021/12/03 14:07:47 by adlecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**"qwcb Hello World! bc"
+**"qwcb"
+**" Hello World! "
+** write(fd, var, len)
+*/
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
---
+++ strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strnstr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adlecler <adlecler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 12:17:27 by adlecler          #+#    #+#             */
+/*   Updated: 2021/12/03 15:25:54 by adlecler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+			{
+				return ((char *)haystack + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
